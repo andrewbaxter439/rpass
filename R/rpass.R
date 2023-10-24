2b2ebd8242dd8a591dc046d4c084d5ea1f0d8c6c
Andy Baxter <andrewbaxter439@gmail.com>
2023-10-24 22:30:38 +0100

    updated, working functions

--- R/rpass.R
+++ R/rpass.R
@@ -3,7 +3,15 @@
 #' @export
 #'
 #' @param nwords Number of words
+#' @param min_l Minimum word length
+#' @param max_l Maximum word length
+#' @param sep Separator between words
+#' @param complex Whether to allow the full dictionary set of complex words
+#' @param seed Set random seed (unset by default)
+#' @param words_dir Directory containing word files
 #'
+#' @importFrom readr read_lines_chunked
+#' @importFrom readr AccumulateCallback
 
 create_pass <- function(nwords = 4,
                         min_l = 4,
@@ -12,16 +20,16 @@ create_pass <- function(nwords = 4,
                         complex = FALSE,
                         seed = NULL,
                         words_dir = "data") {
-  require(readr)
 
   if (!is.null(seed)) set.seed(seed)
 
   file_path <- file.path(words_dir, ifelse(complex, "words.txt", "words_c.txt"))
 
   words <- read_lines_chunked(file_path,
-                              chunk_size = 10000,
+                              chunk_size = 150000,
+                              progress = FALSE,
                               callback = AccumulateCallback$new(
-                                \(x, pos, acc) c(acc, x[grepl(paste0(".{", min_l, ",", max_l, "}"), x)]))
+                                \(x, pos, acc) c(acc, x[grepl(paste0("^.{", min_l, ",", max_l, "}$"), x)]))
   )
 
   sample(words, nwords) |>
