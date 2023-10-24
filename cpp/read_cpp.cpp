963a7c6fb7189e57a4a41e1f4bf5fe17775e93bf
Andy Baxter <andrewbaxter439@gmail.com>
2023-10-24 22:45:26 +0100

    semi-working cpp file

--- cpp/read_cpp.cpp
+++ cpp/read_cpp.cpp
@@ -16,13 +16,14 @@ using namespace Rcpp;
 
 
 // [[Rcpp::export]]
-StringVector read_cpp_len(String file_name, unsigned long min_l, unsigned long max_l) {
+CharacterVector read_cpp_len(String file_name, unsigned long min_l, unsigned long max_l) {
 
-  std::vector<std::string> out_arr;
-  Rcpp::StringVector out_vec;
+  // std::vector<std::string> out_arr;
+  Rcpp::StringVector out_arr;
   // std::vector<char> in_arr = {"a", "aaaaa", "aaaaaa", "aaaaaaaaaa"};
   std::ifstream file(file_name, std::ios::binary);
-
+  // std::vector<char> fileContents((std::istreambuf_iterator<char>(file)),
+  //                                std::istreambuf_iterator<char>());
   // int i = 0;
 
   std::string word;
@@ -31,25 +32,25 @@ StringVector read_cpp_len(String file_name, unsigned long min_l, unsigned long m
     std::istream_iterator<std::string> fileIterator(file);
     std::istream_iterator<std::string> endIterator;
 
-    // while (fileIterator != endIterator) {
-    //   word = *fileIterator;
-    //   if ((word.length() >= min_l) & (word.length() <= max_l)) {
-    //     out_arr.push_back(word);
-    //   }
-    //
-    //   ++fileIterator;
-    //   // ++i;
-    // }
+    while (fileIterator != endIterator) {
+      word = *fileIterator;
+      if ((word.length() >= min_l) & (word.length() <= max_l)) {
+        out_arr.push_back(word);
+      }
 
-    std::copy_if(fileIterator, endIterator, std::back_inserter(out_arr),
-                 [](std::string word) {
-                   return (word.length() >= 4) & (word.length() <= 8);
-                   });
+      ++fileIterator;
+      // ++i;
+    }
+
+    // std::copy_if(fileContents.begin(), fileContents.end(), std::back_inserter(out_arr),
+    //              [](std::string word) {
+    //                return (word.length() >= 4) & (word.length() <= 8);
+    //                });
 
     file.close();
   }
-
-  return Rcpp::StringVector::create(out_arr);
+  // return Rcpp::StringVector::create(out_arr);
+  return out_arr;
 
 }
 
