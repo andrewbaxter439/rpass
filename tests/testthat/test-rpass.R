16eb6f4abe70aa50b7267c32433a577f116ef921
Andy Baxter <andrewbaxter439@gmail.com>
2023-10-25 01:39:33 +0100

    all working!

--- tests/testthat/test-rpass.R
+++ tests/testthat/test-rpass.R
@@ -1,3 +1,3 @@
 test_that("pass output works", {
-  expect_equal(create_pass(seed = 121, words_dir = "C:/Programming/rpass/data"), "notify-although-nasa-magnitude")
+  expect_equal(create_pass(seed = 121, words_dir = "C:/Programming/rpass/data"), "goals-harbour-ethics-rental")
 })
