23bdd0d7a14c755de3be1e103f411aa23cfbd62c
Andy Baxter <andrewbaxter439@gmail.com>
2023-10-25 01:30:36 +0100

    updated buildy bits for cpp inc

--- src/read_cpp.cpp
+++ src/read_cpp.cpp
@@ -4,16 +4,6 @@
 #include <string>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
-
 
 // [[Rcpp::export]]
 StringVector read_cpp_len(String file_name, unsigned long min_l, unsigned long max_l) {
@@ -57,14 +47,3 @@ StringVector read_cpp_len(String file_name, unsigned long min_l, unsigned long m
   return out_vec;
 
 }
-
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
-
-/*** R
-read_cpp_len("data/words_c.txt", 4, 8)
-*/
